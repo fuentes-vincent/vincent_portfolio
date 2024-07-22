c07e992b1bbcb2f0a0b41cb4b0462fa43f1c6dc9
fuentes-vincent <fuentes.vincent96@gmail.com>
2024-07-22 17:26:16 +0800

    Run npm build

--- app/page.jsx
+++ app/page.jsx
@@ -13,10 +13,10 @@ const Home = () => {
         <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Software Developer</span>
           <h1 className="h1 mb-6">
-            Hi I'm <br /> <span className="text-blue-500">Vincent Fuentes</span>
+            Hi I&apos;m <br /> <span className="text-blue-500">Vincent Fuentes</span>
           </h1>
           <p className="max-w-[500px] mb-9 text-white/80">I have 3-4 years of experience as a Software Developer on those various companies</p>
-          {/* button and sociale medias */}
+          {/* button and social media */}
           <div className="flex flex-col xl:flex-row items-center gap-8">
             <Button
             variant="outline"
@@ -35,7 +35,7 @@ const Home = () => {
               </div>
           </div>
         </div>
-           {/*for the photo*/}
+           {/* for the photo */}
             <div className="order-1 xl:order-none mb-8 xl:mb-0">
               <Photo />
             </div>
